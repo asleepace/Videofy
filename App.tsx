a86d75115b6194d953b7feefef6401bbdc1a958d
Colin Teahan <colin_teahan@yahoo.com>
2022-05-10 22:27:21 -0700

    updating the application

--- App.tsx
+++ App.tsx
@@ -1,17 +1,15 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Videofy
+ * Colin Teahan
  *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
+ * Generates a selectable list of thumbnails which can be selected.
  * @format
  */
 
 import React, {useEffect, useState} from 'react'
-import {Image, SafeAreaView, StatusBar, StyleSheet} from 'react-native'
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native'
 import {ReadDirItem} from 'react-native-fs'
-import {Timeline} from './src/components/timeline'
+import {Selected, Timeline} from './src/components'
 import {getThumbnails} from './src/utils'
 
 const App = () => {
@@ -33,55 +31,17 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'dark-content'} />
-      <Image
-        source={{uri: `file://${selected?.path}`}}
-        style={styles.mainImage}
-        resizeMode={'cover'}
-      />
+      <Selected selected={selected} />
       <Timeline selected={selected} items={files} onSelect={setSelected} />
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  frame: {
-    flexDirection: 'row',
-    borderRadius: 8,
-    overflow: 'hidden',
-    width: '100%',
-    height: 60,
-    padding: 16,
-  },
-  image: {
-    height: 60,
-    flex: 1,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
   container: {
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-  },
-  mainImage: {
-    backgroundColor: '#EEE',
-    borderRadius: 8,
-    height: 400,
-    width: 240,
   },
 })
 
